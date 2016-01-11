894cbe7e1b49a06829a23a8836b6967f275e3889
htroyer <htroyer@uchicago.edu>
2016-01-11 09:51:33 -0600

    Merge remote-tracking branch 'origin/phillis' into phillis
    # Conflicts:
    #	compiled/about.html
    #	compiled/bcc.html
    #	compiled/contact.html
    #	compiled/education.html
    #	compiled/index.html
    #	compiled/leadership.html
    #	compiled/static/css/font.css
    #	compiled/static/font/icomoon/icomoon.svg
    #	sync_s3.sh
    #	template/about.html
    #	template/education.html
    #	template/index.html

--- sync_s3.sh
+++ sync_s3.sh
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 aws s3 sync compiled/ s3://cdis
+=======
+aws s3 sync compiled/ s3://$1
+>>>>>>> origin/phillis
+=======
+aws s3 sync compiled/ s3://$1
+>>>>>>> origin/phillis
